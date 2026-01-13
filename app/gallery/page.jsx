6e52b770eac65de0ba38ebb10a90005e109091fe
Sunanda-05 <mannasunanda11705@gmail.com>
2026-01-14 00:54:32 +0530

    chore: gallery

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,17 +1,26 @@
 import VerticalTimeline from '@/components/sections/gallery/Timeline';
+import TornEdge0 from '@/components/shared/edges/TornEdge0';
+import TornEdge2 from '@/components/shared/edges/TornEdge2';
 import Header from '@/components/shared/texts/Header';
 import React from 'react';
 
 const Gallery = () => {
   return (
-    <div className="bg-cream">
+    <div className="bg-sand-light">
       <Header
         heading="Gallery"
-        bgColorClass={'bg-cream'}
-        textColorClass={'text-cream'}
+        bgColorClass={'bg-sand-light'}
+        textColorClass={'text-sand'}
         brushColor={'#000000'}
       />
       <VerticalTimeline />
+      <TornEdge0
+        version="rotated"
+        upSectionColor="#000000"
+        edgeColor="#ffffff"
+        className="overflow-hidden"
+        downSectionColor="#faf5ee"
+      />
     </div>
   );
 };
--- components/sections/home/sponsors/Marquee.jsx
+++ components/sections/home/sponsors/Marquee.jsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useRef, useLayoutEffect, useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion } from 'motion/react';
 
 const MarqueeAnimation = ({ children, direction, duration, className }) => {
   const containerRef = useRef(null);
