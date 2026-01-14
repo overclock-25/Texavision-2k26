52c2b3cf64545ad4f78e8848fd675c673e3eb0b2
Anuvab Maity <162330937+AnuvabMaity@users.noreply.github.com>
2026-01-14 12:34:33 +0530

    feat(#8): Added hero section (#36)
    * feat: added hero section
    
    * fix: code review suggestions

--- components/sections/home/hero/Hero.jsx
+++ components/sections/home/hero/Hero.jsx
@@ -1,41 +1,56 @@
-import { TextAnchor } from '@/components/shared/texts/Anchor';
+'use client';
+import { SplashGif } from '@/components/sections/home/hero/SplashGif';
+import { CountdownTimer } from '@/components/sections/home/hero/Timer';
+import Logo from '@/public/hero/texavision.svg';
+
+import { useIsMobile } from '@/hooks/useMobile';
+
+const DELAY = 3000;
 
 const Hero = () => {
+  const isMobile = useIsMobile();
+
   return (
-    <div className="h-screen w-full">
-      <p className="text-center">
-        {' '}
-        Dashing through the snow,
-        <br />
-        In a one-horse open sleigh,
-        <br />
-        O&apos;er the fields we go,
-        <br />
-        Laughing all the way;
-        <br />
-        Bells on bobtail ring,
-        <br />
-        Making spirits bright,
-        <br />
-        What fun it is to ride and sing
-        <br />
-        <TextAnchor
-          href="https://youtu.be/fMCN-b0ic_k?si=pcH-CkVJl4J1bzyn"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Very cool anchor link
-        </TextAnchor>
-        <br />
-        Jingle bells, jingle bells,
-        <br />
-        Jingle all the way!
-        <br />
-        Oh, what fun it is to ride
-        <br />
-        In a one-horse open sleigh!
-        <br />
-      </p>
+    <div className="relative h-screen w-full overflow-hidden">
+      <div
+        className="absolute top-0 left-0 -z-10 h-full w-full bg-cover bg-center brightness-40"
+        style={{ backgroundImage: `url('hero/bg.jpg')` }}
+      ></div>
+
+      <SplashGif
+        className="top-1/2 left-1/3 z-0 h-screen w-[90vw] min-w-[600px] -translate-x-1/2 -translate-y-1/2"
+        delay={DELAY}
+        maskImage="hero/1/mask.gif"
+        bgImage="hero/1/img.jpg"
+      />
+
+      {!isMobile && (
+        <SplashGif
+          className="-top-10 right-10 z-10 h-[80vh] w-[60vh]"
+          delay={DELAY + 1000}
+          maskImage="hero/2/mask.gif"
+          bgImage="hero/2/img.jpg"
+        />
+      )}
+
+      <SplashGif
+        className="top-0 left-0 z-10 h-[45vh] w-[40vw] min-w-[250px]"
+        delay={DELAY + 1500}
+        maskImage="hero/3/mask.gif"
+        bgImage="hero/3/img.jpg"
+      />
+
+      <SplashGif
+        className="right-0 -bottom-40 z-10 h-[70vh] w-[40vw] min-w-[400px] scale-x-[-1] md:right-20 md:-bottom-20"
+        delay={DELAY + 1800}
+        maskImage="hero/4/mask.gif"
+        bgImage="hero/4/img.jpg"
+      />
+
+      <div className="hero absolute z-200 flex h-screen w-full flex-col items-center justify-center gap-10">
+        <Logo className="h-auto w-[80vw] max-w-200" />
+        <CountdownTimer target={'2026-01-25T00:00:00+05:30'} delay={DELAY} />
+      </div>
     </div>
   );
 };
